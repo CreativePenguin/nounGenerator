65bcec9541ff2f0c9825060125030c29835a9d04
Derek Leung <dleung00@stuy.edu>
2019-11-20 14:00:28 -0500

    updated stuf

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # nounGenerator -- Derek Leung, Winston Peng, and Ahmed Sultan
 # SoftDev1 pd 9
-# P01 -- 
+# P01 --
 # 2019-11-14
 
 from flask import Flask
@@ -11,6 +11,23 @@ import urllib
 import json
 
 app = Flask(__name__)
+
+#-----------------------------------------------------------------
+#DATABASE SETUP
+DB_FILE = "Info.db"
+db = sqlite3.connect(DB_FILE)
+c = db.cursor()
+c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='userdata' ''')
+if c.fetchone()[0] < 1:
+    c.execute("CREATE TABLE userdata();")
+c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='countrydata' ''')
+if c.fetchone()[0] < 1:
+    c.execute("CREATE TABLE countrydata();")
+c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='scores' ''')
+if c.fetchone()[0] < 1:
+    c.execute("CREATE TABLE scores();")
+
+#-----------------------------------------------------------------
 
 @app.route("/")
 def main():
