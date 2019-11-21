928d4bb0ba13aae4be0f7e3aabee793fc3e9c048
Derek Leung <dleung00@stuy.edu>
2019-11-21 14:53:20 -0500

    added stuf

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ from flask import url_for
 import urllib
 import json
 
-app = Flask(__name__)
-
 #-----------------------------------------------------------------
 #DATABASE SETUP
 DB_FILE = "Info.db"
@@ -19,19 +17,23 @@ db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='userdata' ''')
 if c.fetchone()[0] < 1:
-    c.execute("CREATE TABLE userdata();")
+    c.execute("CREATE TABLE userdata(username TEXT, password TEXT);")
 c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='countrydata' ''')
 if c.fetchone()[0] < 1:
-    c.execute("CREATE TABLE countrydata();")
+    c.execute("CREATE TABLE countrydata(countryname TEXT, population INTEGER, capital TEXT, demonym TEXT, flag_url BLOB, languages BLOB);")
 c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='scores' ''')
 if c.fetchone()[0] < 1:
-    c.execute("CREATE TABLE scores();")
+    c.execute("CREATE TABLE scores(username TEXT, score1 INTEGER, score2 INTEGER, score3 INTEGER, score4 INTEGER, score5 INTEGER);")
 
 #-----------------------------------------------------------------
+#FLASK STUFF
 
-@app.route("/")
-def main():
-    return render_template("base.html")
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+@app.route("/", methods=['GET', 'POST'])
+def checkCreds():
+    return redirect("/login")
 
 @app.route("/login")
 def login():
@@ -40,6 +42,15 @@ def login():
 @app.route("/register")
 def reigster():
     return render_template("register.html")
+
+@app.route("/home")
+def home():
+    return None
+
+@app.route("/leaderboards")
+def leaderboards():
+    return None
+
 
 if __name__ == "__main__":
     app.debug = True
